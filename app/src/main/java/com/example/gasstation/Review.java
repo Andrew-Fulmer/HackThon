2bf0be7160b5e5a3e1fc686689c7f2834a600e53
ScottMariotte <Scottjmariotte@mail.com>
2019-10-13 06:16:52 -0400

    Added ability to view some information on bathrooms

--- app/src/main/java/com/example/gasstation/Review.java
+++ app/src/main/java/com/example/gasstation/Review.java
@@ -1,10 +1,13 @@
 package com.example.gasstation;
 
-public class Review {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Review implements Parcelable {
     // Review Text
     private String rText = "";
     // Which tags (finite number, in array
-    private boolean[] rTags;
+    private boolean[] rTags ;
 
     private double rOverall = 0;
     private double rSafety = 0;
@@ -17,6 +20,26 @@ public class Review {
         rSafety = safety;
         rClean = clean;
     }
+
+    protected Review(Parcel in) {
+        rText = in.readString();
+        rTags = in.createBooleanArray();
+        rOverall = in.readDouble();
+        rSafety = in.readDouble();
+        rClean = in.readDouble();
+    }
+
+    public static final Creator<Review> CREATOR = new Creator<Review>() {
+        @Override
+        public Review createFromParcel(Parcel in) {
+            return new Review(in);
+        }
+
+        @Override
+        public Review[] newArray(int size) {
+            return new Review[size];
+        }
+    };
 
     public String comment(){
         return rText;
@@ -32,6 +55,20 @@ public class Review {
     }
     public double clean() {
         return rClean;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(rText);
+        dest.writeBooleanArray(rTags);
+        dest.writeDouble(rOverall);
+        dest.writeDouble(rSafety);
+        dest.writeDouble(rClean);
     }
 }
 
