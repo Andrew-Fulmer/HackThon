e962a90331282176e11d4cb28bf48c835a06c16e
ScottMariotte <Scottjmariotte@mail.com>
2019-10-13 05:05:19 -0400

    Added display location xml / bathroom parceble

--- app/src/main/java/com/example/gasstation/DisplayLocation.java
+++ app/src/main/java/com/example/gasstation/DisplayLocation.java
@@ -1,6 +1,7 @@
 package com.example.gasstation;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 
@@ -8,14 +9,18 @@ public class DisplayLocation extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.displaylocation);
+
+        Intent intent = getIntent();
+        Bathroom bathroom = intent.getParcelableExtra("bathroom");
+
+
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
         int width = dm.widthPixels;
         int height = dm.heightPixels;
-        getWindow().setLayout((int)(width*.8),(int)(height*.6));
+        getWindow().setLayout(width,height);
     }
 }
